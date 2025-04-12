97d62756230c191f2787cd0fd8d4fe1b4f3fe1fb
Douglas Valério <dglvalerio@gmail.com>
2025-04-12 09:29:20 -0300

    feat: add inut validation on input fields - bmi data input

--- lib/bmi_controller.dart
+++ lib/bmi_controller.dart
@@ -22,4 +22,31 @@ class BMIController extends ChangeNotifier {
 
     notifyListeners();
   }
+  String? validateWeight(String? value) {
+    final weightStr = value ?? '';
+    if (double.tryParse(weightStr) == null) {
+      return "Please, enter a valid weight!";
+    } else if (double.parse(weightStr) <= 0) {
+      return "Weight must be greater than zero!";
+    } else if (double.parse(weightStr) > 200) {
+      return "Weight must be less than 200!";
+    } 
+    else {
+      return null;
+    }
+  }
+
+  String? validateHeight(String? value) {
+    final heightStr = value ?? '';
+     if (double.tryParse(heightStr) == null) {
+      return "Please, enter a valid height!";
+    } else if (double.parse(heightStr) <= 0) {
+      return "Height must be greater than zero!";
+    } else if (double.parse(heightStr) > 200) {
+      return "Height must be less than 200!";
+    }    
+    else {
+      return null;
+    }
+  }
 }
